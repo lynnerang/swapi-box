6527a3ccfb48c131ca82389b2eb820e63e9f61d4
Lynne Rang <lynne.rang@gmail.com>
2019-05-05 22:01:12 -0600

    Add modular folder structure and favorite bugfix.

--- src/Components/DataScreen/DataScreen.js
+++ src/Components/DataScreen/DataScreen.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import './DataScreen.scss';
 import { NavLink } from 'react-router-dom';
-import Card from './Card';
+import Card from '../Card/Card.js';
 
 function DataScreen(props) {
   const cards = props.data.map(item => <Card data={item} key={item.name} refreshFavCount={props.refreshFavCount}/>);
@@ -11,15 +11,15 @@ function DataScreen(props) {
     <div className='content'>
       <nav className='data-nav'>
         <NavLink className='category-option' to='/people' onClick={props.getPeopleData}>
-          <img className='category-img-small' src={require('./images/peopleicon.png')} aria-hidden='true'/>
+          <img className='category-img-small' src={require('../../images/peopleicon.png')} aria-hidden='true'/>
           <p className='category-text-small'>People</p>
         </NavLink>
         <NavLink className='category-option' to='/planets' onClick={props.getPlanetData}>
-          <img className='category-img-small' src={require('./images/planeticon.png')} aria-hidden='true'/>
+          <img className='category-img-small' src={require('../../images/planeticon.png')} aria-hidden='true'/>
           <p className='category-text-small'>Planets</p>
         </NavLink>
         <NavLink className='category-option' to='/vehicles' onClick={props.getVehicleData}>
-          <img className='category-img-small' src={require('./images/vehiclesicon.png')} aria-hidden='true'/>
+          <img className='category-img-small' src={require('../../images/vehiclesicon.png')} aria-hidden='true'/>
           <p className='category-text-small'>Vehicles</p>
         </NavLink>
       </nav>
--- src/Components/ExploreScreen/ExploreScreen.js
+++ src/Components/ExploreScreen/ExploreScreen.js
@@ -8,15 +8,15 @@ function ExploreScreen(props) {
       <h2 className='category-header'>CHOOSE A CATEGORY</h2>
       <div className='category-options'>
         <NavLink className='category-card' to='/people' onClick={props.getPeopleData}>
-          <img className='category-img' src={require('./images/peopleicon.png')}aria-hidden='true' alt='darth vader icon'/>
+          <img className='category-img' src={require('../../images/peopleicon.png')}aria-hidden='true' alt='darth vader icon'/>
           <p className='category-text'>People</p>
         </NavLink>
         <NavLink className='category-card' to='/planets' onClick={props.getPlanetData}>
-          <img className='category-img' src={require('./images/planeticon.png')}aria-hidden='true' alt='planet icon' />
+          <img className='category-img' src={require('../../images/planeticon.png')}aria-hidden='true' alt='planet icon' />
           <p className='category-text'>Planets</p>
         </NavLink>
         <NavLink className='category-card' to='/vehicles'>
-          <img className='category-img' src={require('./images/vehiclesicon.png')} aria-hidden='true' alt='millenium falcon icon'/>
+          <img className='category-img' src={require('../../images/vehiclesicon.png')} aria-hidden='true' alt='millenium falcon icon'/>
           <p className='category-text'>Vehicles</p>
         </NavLink>
       </div>
